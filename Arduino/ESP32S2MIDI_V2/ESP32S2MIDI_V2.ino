41cea7b0b742246d528272ec6a691e7fbef4c6b4
Jildert Viet <jivido@hotmail.com>
2020-11-28 12:21:56 +0100

    Removed comments and multipsampling feature

--- Arduino/ESP32S2MIDI_V2/ESP32S2MIDI_V2.ino
+++ Arduino/ESP32S2MIDI_V2/ESP32S2MIDI_V2.ino
@@ -3,17 +3,12 @@
 #include "WiFi.h"
 #include <SimpleKalmanFilter.h>
 
-#define MULTI_DEPTH 300
-
 MIDIusb midi;
 
 char adcPins[5] = {5, 4, 3, 2, 1};
 char prevCC[5];
 
-int multisampling[5][MULTI_DEPTH];
-int multisamplingWritePos = 0;
 SimpleKalmanFilter* filters[5];
-bool bBufferFilled = false; // Set this after the buffer is filled for the first time
 
 long refresh_time; // Temp
 bool bTest;
@@ -29,59 +24,15 @@ void setup() {
     pinMode(adcPins[i], INPUT);
     filters[i] = new SimpleKalmanFilter(0.1, 0.1, 0.01);
     prevCC[i] = 0;
-    for (int j = 0; j < MULTI_DEPTH; j++) {
-      multisampling[i][j] = 0;
-    }
   }
 }
 
 void loop() {
-//  bool bUpdate = false;
-//  if (millis() > refresh_time) {
-//    bUpdate = true;
-//  }
-
   for (int i = 0; i < 5; i++) {
-//    multisampling[i][multisamplingWritePos] = analogRead(adcPins[i]);
-
     float value = filters[i]->updateEstimate(analogRead(adcPins[i]) / 1600.0);
     char cc = value * 128;
     if(prevCC[i] != cc)
       midi.controlChange(i, cc, 0);
     prevCC[i] = cc;
-
-//    if(bUpdate){
-//      Serial.print((int)cc); Serial.print("\t");
-//    }
-    
-    // Get mode of buffer
-//    if (bBufferFilled) {
-//      int total = 0;
-//      for (int j = 1; j < MULTI_DEPTH; j++)
-//        total += multisampling[i][j];
-//
-//      total /= (float)MULTI_DEPTH;
-//      char cc = (total / 1600.0) * 128;
-//      if (cc != prevCC[i]) {
-//        // Send CC
-//        midi.controlChange(i, cc, 0);
-        
-//      }
-//      prevCC[i] = cc;
-//    }
   }
-//  if (bUpdate) {
-//    Serial.println();
-//    refresh_time = millis() + 50;
-//  }
-  
-//
-//  multisamplingWritePos++;
-//  if (multisamplingWritePos >= MULTI_DEPTH) {
-//    if (!bBufferFilled)
-//      bBufferFilled = true;
-//    multisamplingWritePos = 0; // reset
-//  }
-  
-//  delayMicroseconds(10);
 }
